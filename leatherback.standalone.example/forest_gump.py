394599784729f3d5f11d88f4a06a2800b796a67f
Rob Murrer <robmurrer@gmail.com>
2025-07-15 09:13:36 -0500

    Implement physics step logic and initialize robot articulation in forest_gump script

--- leatherback.standalone.example/forest_gump.py
+++ leatherback.standalone.example/forest_gump.py
@@ -12,14 +12,24 @@ from isaacsim.core.utils.prims import define_prim, get_prim_at_path
 from leatherback.policy.example.leatherback import LeatherbackPolicy
 from isaacsim.storage.native import get_assets_root_path
 
+
+
 script_dir = os.path.dirname(__file__)
 relative_path = os.path.join("..", "leatherback")
 full_path = os.path.abspath(os.path.join(script_dir, relative_path))
 usd_path = os.path.abspath(os.path.join(full_path, "leatherback_simple_better.usd"))
 
 
+step_angle = np.pi / 180 * 30  # 5 degrees
+phys_i = 0
 def on_physics_step(step_size) -> None:
-    pass
+    global phys_i
+    global step_angle
+    phys_i += 1
+    if phys_i % 100 == 0:
+        step_angle = -step_angle  # reverse direction every 10 steps
+    robot_art.set_joint_positions([[0, 0, step_angle, -step_angle, 0, 0]])
+    
 
 
 my_world = World(stage_units_in_meters=1.0, physics_dt=1 / 60, rendering_dt=1 / 50)
@@ -29,7 +39,29 @@ prim = define_prim("/World/Ground", "Xform")
 asset_path = assets_root_path + "/Isaac/Environments/Grid/default_environment.usd"
 prim.GetReferences().AddReference(asset_path)
 
-my_world.reset()
+
+lb_prim_path = "/World/Leatherback"
+from isaacsim.core.utils.stage import add_reference_to_stage
+add_reference_to_stage(usd_path, lb_prim_path)
+
+from isaacsim.core.prims import Articulation
+robot_art = Articulation(prim_paths_expr=lb_prim_path, name="Leatherback")
+
+robot_art.set_world_poses(positions=np.array([[0,0,0.5]]))
+my_world.reset() # required to have joints available
+
+#print("joint names: ", robot_art.joint_names)
+# just for ordering reference
+wheel_rr = 'Wheel__Upright__Rear_Right'
+wheel_rl = 'Wheel__Upright__Rear_Left'
+knuckle_fr = 'Knuckle__Upright__Front_Right'
+knuckle_fl = 'Knuckle__Upright__Front_Left'
+wheel_knuckle_fr = 'Wheel__Knuckle__Front_Right'
+wheel_knuckle_fl = 'Wheel__Knuckle__Front_Left'
+
+wheel_vel = 6
+robot_art.set_joint_velocities([[wheel_vel, wheel_vel, 0, 0, wheel_vel, wheel_vel]])
+
 my_world.add_physics_callback("physics_step", callback_fn=on_physics_step)
 
 while simulation_app.is_running():
